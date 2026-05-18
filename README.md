fd762b3c2303a55b951d6c94e9866100168626f6
Apeksha A N <anapeksha07@gmail.com>
2026-05-17 19:07:56 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Medical-Chatbot
 Medical Chatbot with LLM, Aws
+
+
+# clone repo
+git clone 'repo link'
+
+# create virtual env
+conda create -n medibot python=3.10 -y
+
+conda activate medibot
+
+
+# install the requirements
+
+pip install -r requirements.txt
+
+
+
+
+
+
+
+
---
+++ template.sh
@@ -0,0 +1,18 @@
+
+# Creating directories
+mkdir -p src
+mkdir -p research
+
+#Creating files 
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch research/trials.ipynb
+touch research/analysis.ipynb
+touch requirements.txt
+touch setup.py
+
+
+echo "Directories and files created successfully!"
